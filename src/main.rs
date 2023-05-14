aae69054cd479423a9576ad6e123c52f9a017888
Kdwk <kdwkleung@proton.me>
2023-05-14 23:26:00 +0800

    Used libadwaita; Made buttons circular

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use relm4::{prelude::*, gtk::{traits::{GtkWindowExt, BoxExt, ButtonExt}, glib::clone, subclass::text_view}, adw::prelude::*};
+use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
+use relm4::{gtk::{self, traits::WidgetExt}, ComponentParts, ComponentSender, RelmApp, RelmWidgetExt, SimpleComponent};
 
 #[derive(Debug)]
 enum AppInput {
@@ -22,75 +23,39 @@ impl SimpleComponent for AppModel {
     type Output = ();
     /// The type of data with which this component will be initialized.
     type Init = isize;
-    /// The root GTK widget that this component will create.
-    type Root = gtk::Window;
-    /// A data structure that contains the widgets that you will need to update.
-    // type Widgets = AppWidgets;
-    // fn init_root() -> Self::Root {
-    //     adw::Window::builder()
-    //         .title("Counter")
-    //         .default_height(700)
-    //         .default_width(650)
-    //         .build()
-    // }
-
-    // fn init(
-    //         counter: Self::Init,
-    //         window: &Self::Root,
-    //         sender: ComponentSender<Self>,
-    //     ) -> ComponentParts<Self> {
-    //     let model = AppModel {counter};
-    //     let vbox = gtk::Box::builder()
-    //                     .orientation(gtk::Orientation::Vertical)
-    //                     .spacing(5)
-    //                     .build();
-    //     let inc_button = gtk::Button::builder()
-    //                         .label("Increment")
-    //                         .build();
-    //     let dec_button = gtk::Button::builder()
-    //                         .label("Decrement")
-    //                         .build();
-    //     let label = gtk::Label::new(Some(&format!("{}", model.counter)));
-
-    //     vbox.append(&label);
-    //     vbox.append(&inc_button);
-    //     vbox.append(&dec_button);
-    //     window.set_child(Some(&vbox));
-        
-    //     inc_button.connect_clicked(clone!(@strong sender => move |_| {
-    //         sender.input(AppInput::Increment);
-    //     }));
-    //     dec_button.connect_clicked(clone!(@strong sender => move |_| {
-    //         sender.input(AppInput::Decrement);
-    //     }));
-
-    //     let widgets = AppWidgets {label};
-
-    //     ComponentParts { model: model, widgets: widgets }
-    // }
 
     view! {
         gtk::Window {
             set_title: Some("Counter"),
-            set_default_height: 300,
-            set_default_width: 250,
+            set_default_height: 200,
+            set_default_width: 200,
 
             gtk::Box {
                 set_orientation: gtk::Orientation::Vertical,
-                set_spacing: 5,
-                set_margin_all: 5,
+                set_spacing: 10,
+                set_margin_all: 20,
 
                 gtk::Label {
                     #[watch]
                     set_label: &format!("{}", model.counter)
                 },
-                gtk::Button {
-                    set_label: "Increment",
-                    connect_clicked => Increment
-                },
-                gtk::Button {
-                    set_label: "Decrement",
-                    connect_clicked => Decrement
+
+                gtk::Box {
+                    set_orientation: gtk::Orientation::Horizontal,
+                    set_halign: gtk::Align::Center,
+                    set_spacing: 5,
+                    set_margin_all: 5,
+
+                    gtk::Button {
+                        add_css_class: "circular",
+                        set_icon_name: "list-add-symbolic",
+                        connect_clicked => AppInput::Increment
+                    },
+                    gtk::Button {
+                        add_css_class: "circular",
+                        set_icon_name: "list-remove-symbolic",
+                        connect_clicked => AppInput::Decrement
+                    }
                 }
             }
 
@@ -98,13 +63,13 @@ impl SimpleComponent for AppModel {
     }
 
     fn init(
-            init: Self::Init,
+            counter: Self::Init,
             root: &Self::Root,
             sender: ComponentSender<Self>,
         ) -> ComponentParts<Self> {
         let model = AppModel{counter};
         let widgets = view_output!();
-        ComponentParts { model: model, widgets: widgets };
+        ComponentParts { model: model, widgets: widgets }
     }
 
     fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
@@ -116,12 +81,6 @@ impl SimpleComponent for AppModel {
                 self.counter -= 1;
             }
         }
-    }
-
-    fn update_view(&self, widgets: &mut Self::Widgets, sender: ComponentSender<Self>) {
-        let counter_string = self.counter.to_string();
-        let counter_str: &str = &counter_string;
-        widgets.label.set_label(counter_str);
     }
 }
 
