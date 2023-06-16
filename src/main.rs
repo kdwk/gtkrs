9a5acee897859b46778a2ff538c085574ec370a8
Kdwk <kdwkleung@proton.me>
2023-06-16 10:59:13 +0800

    WebView experiment

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,44 @@
 #![allow(unused_imports)]
 #![allow(unused_variables)]
-use relm4::gtk::{prelude::*, Box, Label};
-use relm4::adw::{prelude::*, Window, HeaderBar, MessageDialog, ViewStack};
+use relm4::gtk::{prelude::*, Box, Label, Button, Orientation, Align, Video};
+use relm4::adw::{prelude::*, Window, HeaderBar, MessageDialog, ViewStack, StatusPage};
 use relm4::prelude::*;
 use relm4_macros::*;
-use gtkrs::{header::{Header}, dialog::{Dialog, DialogOutput, DialogInput}, stack::{Stack}};
+use webkit6::{prelude::*, WebView};
 
 struct App {
-    header: Controller<Header>,
-    dialog: Option<Controller<Dialog>>,
-    stack: Controller<Stack>,
+    uri: String,
 }
 
 #[derive(Debug)]
 enum AppInput {
-    CloseDialog,
-    CloseRequest,
-    CloseWindow,
+    PlaceHolder,
 }
 
 #[relm4::component]
-impl Component for App {
+impl SimpleComponent for App {
     type Init = ();
     type Input = AppInput;
     type Output = ();
-    type CommandOutput = ();
 
     view! {
         Window {
-            set_default_width: 500,
-            set_default_height: 500,
+            set_default_height: 1000,
+            set_default_width: 1000,
+            set_title: Some(""),
 
             Box {
-                set_orientation: gtk::Orientation::Vertical,
-                model.header.widget(),
-                model.stack.widget(),
-            },
-            
-            connect_close_request[sender] => move |_| {
-                sender.input(AppInput::CloseRequest);
-                gtk::Inhibit(true)
+                set_orientation: Orientation::Vertical,
+                
+                // HeaderBar {
+                //     set_decoration_layout: Some(":close"),
+                //     add_css_class: "flat",
+                // },
+
+                WebView {
+                    set_vexpand: true,
+                    load_uri: model.uri.as_str(),
+                }
             }
         }
     }
@@ -49,39 +48,20 @@ impl Component for App {
             root: &Self::Root,
             sender: ComponentSender<Self>,
         ) -> ComponentParts<Self> {
-        let dialog: Option<Controller<Dialog>> = None;
-        let stack: Controller<Stack> = Stack::builder().launch(()).detach();
-        let stack_ref: Option<ViewStack> = Some(stack.widget().clone());
-        let header: Controller<Header> = Header::builder()
-                                                    .launch(stack_ref)
-                                                    .detach();
-        let model = App { header: header, dialog: dialog, stack: stack };
+        let model = App { uri: String::from("https://apple.com")};
         let widgets = view_output!();
         ComponentParts { model: model, widgets: widgets }
     }
 
-    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>, root: &Self::Root) {
+    fn update(&mut self, message: Self::Input, sender: ComponentSender<Self>) {
         match message {
-            AppInput::CloseRequest => {
-                self.dialog = Some(Dialog::builder()
-                                                .transient_for(root)
-                                                .launch(())
-                                                .forward(sender.input_sender(), |message| match message {
-                                                    DialogOutput::CloseDialog => AppInput::CloseDialog,
-                                                    DialogOutput::CloseWindow => AppInput::CloseWindow,
-                                                }));
-            },
-            AppInput::CloseDialog => self.dialog = None,
-            AppInput::CloseWindow => {
-                self.dialog = None;
-                relm4::main_application().quit()
-            },
+            AppInput::PlaceHolder => {},
         }
     }
 }
 
 fn main() {
-    let app = RelmApp::new("com.github.kdwk.Dialog");
+    let app = RelmApp::new("com.github.kdwk.Web");
     relm4_icons::initialize_icons();
     app.run::<App>(());
 }
